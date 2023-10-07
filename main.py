bdf1bcd0bdfe80a500a72fcace8735f46d3bfdad
Chase Callahan <ctcallahan2@crimson.ua.edu>
2023-10-07 13:27:59 -0500

    Added use reviews

--- main.py
+++ main.py
@@ -4,30 +4,6 @@ from operator import itemgetter
 # Schemas used for validation and reading in from files
 
 
-review_schema = {
-    "id": {
-        "fromFile": lambda x: int(x),
-        "col": 0
-    },
-    "vehicle_name": {
-        "fromFile": lambda x: x,
-        "col": 1
-    },
-    "date": {
-        "fromFile": lambda x: datetime.strptime(x, "%Y-%m-%d %H:%M:%S"),
-        "col": 2
-    },
-    "rating": {
-        "fromFile": lambda x: int(x),
-        "col": 3
-    },
-    "comment": {
-        "fromFile": lambda x: x,
-        "col": 4
-    }
-}
-
-
 vehicles = []
 reviews = []
 
@@ -114,6 +90,126 @@ def use_vehcile():
     vehicles = read_file("cars", schema)
 
     def validate_name(name):
+        if(len(name) <= 1):
+            return validate_name(input("Please enter a name of at least one character: "))
+        if(len(list(filter(lambda vehicle: vehicle[0] == name, vehicles))) > 0):
+            return validate_name(input("Please enter a name that is not already taken: "))
+        return name
+    
+    schema["name"]["validate"] = validate_name
+
+    
+    def validate_type(type):
+        return type
+    
+    schema["type"]["validate"] = validate_type
+
+    
+    def validate_man_year(year):
+        try:
+            return int(year)
+        except ValueError:
+            return validate_man_year(input("Please enter a valid number: "))
+        
+    schema["manufactureYear"]["validate"] = validate_man_year
+    
+        
+    def validate_price(price):
+        try:
+            return float(price)
+        except ValueError:
+            return validate_price(input("Please enter a valid price: "))
+        
+    schema["price"]["validate"] = validate_price
+
+    def find_vehicle():
+        display_items(vehicles, schema)
+        search_val = input("Insert the name of the vehicle you are searching for: ")
+        found_vehicle = find_item(search_val, schema, vehicles)
+        if(found_vehicle == None and input(f"Vehicle with name: {search_val} not found, continue? (y/n): ") == "y"):
+            return find_item()
+        return found_vehicle
+        
+
+    def add_vehicle():
+        vehicle = []
+        display_items(vehicles, schema)
+        for name, field in schema.items():
+            # Verifies input entered in validation function defined in schema
+            vehicle.append(field["validate"](input(f"Please enter a value for {name}: ")))
+        
+        vehicles.append(vehicle)
+
+        write_file("cars", vehicles)
+
+    def edit_vehicle():
+        found_vehicle = find_vehicle()
+        
+        if(found_vehicle == None): 
+            return
+        
+        new_vehicle = []
+        for name, field in schema.items():
+            # Verifies input entered in validation function defined in schema
+            if(input(f"{name}: {found_vehicle[schema[name]['col']]}\nEnter 'y' to edit: ").lower() == "y"):
+                new_vehicle.append(field["validate"](input(f"Please enter a value for {name}: ")))
+            else:
+                new_vehicle.append(found_vehicle[schema[name]['col']])
+        
+        vehicles.remove(found_vehicle)
+        vehicles.append(new_vehicle)
+        write_file("cars", vehicles)
+
+    def delete_vehicle():
+        found_vehicle = find_vehicle()
+
+        if(found_vehicle == None):
+            return None
+        
+        print(f"\nWould you like to remove this vehicle?\n\n{prep_display_item(found_vehicle, schema)}\n")
+        if(input("(Please enter y/n): ").lower() == "y"):
+            print("\nRemoving vehicle...")
+            pause()
+            vehicles.remove(found_vehicle)
+            write_file("cars", vehicles)
+
+
+    # Expose vehcile API functions
+    return [
+        vehicles,
+        add_vehicle,
+        edit_vehicle,
+        delete_vehicle
+    ]
+
+def use_reviews():
+    schema = {
+        "id": {
+            "fromFile": lambda x: int(x),
+            "col": 0,
+            "validate": validate_id
+        },
+        "vehicle_name": {
+            "fromFile": lambda x: x,
+            "col": 1
+        },
+        "date": {
+            "fromFile": lambda x: datetime.strptime(x, "%Y-%m-%d %H:%M:%S"),
+            "col": 2
+        },
+        "rating": {
+            "fromFile": lambda x: int(x),
+            "col": 3
+        },
+        "comment": {
+            "fromFile": lambda x: x,
+            "col": 4
+        }
+    }
+
+    reviews = read_file("reviews", schema)
+
+    def validate_id(id):
         if(len(name) <= 1):
             return validate_name(input("Please enter a name of at least one character: "))
         if(len(list(filter(lambda vehicle: vehicle[0] == name, vehicles))) > 0):
